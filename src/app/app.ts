cafc209588f8e23fcf86652cb3b1d070abc57ef2
induma2002 <induma2002@users.noreply.github.com>
2025-08-12 19:41:06 +0530

    UI impliment Student and Locker

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Sidebar } from "./sidebar/sidebar";
+import { Student } from "./student/student";
+import { Locker } from "./locker/locker";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Sidebar, Student, Locker],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/locker/locker.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-locker',
+  imports: [],
+  templateUrl: './locker.html',
+  styleUrl: './locker.css'
+})
+export class Locker {
+
+}
