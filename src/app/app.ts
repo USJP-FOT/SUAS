e3d764a5274c0af8e2c497d5bc88021977cafd1f
Induma Wijesinghe <115974206+induma2002@users.noreply.github.com>
2025-08-16 02:23:23 +0530

    feat: add responsive theme support

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Sidebar } from "./sidebar/sidebar";
 import { Student } from "./student/student";
@@ -10,6 +10,20 @@ import { Locker } from "./locker/locker";
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected title = 'suas';
+  isDark = false;
+
+  ngOnInit() {
+    this.applyTheme();
+  }
+
+  toggleTheme() {
+    this.isDark = !this.isDark;
+    this.applyTheme();
+  }
+
+  private applyTheme() {
+    document.body.classList.toggle('dark-theme', this.isDark);
+  }
 }
